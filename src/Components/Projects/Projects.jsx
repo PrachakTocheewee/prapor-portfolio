ce78c922b559a7891648f9eb8a4bc1d708bb5251
Prachak Tocheewee <prachak.toc@gmail.com>
2022-09-02 05:06:07 +0700

    Split all components

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,77 +1,74 @@
-import React from 'react';
+import React from 'react'
 import './Projects.css'
 
 function Projects() {
-    return (
-        <div>
-            <div className="section">
-					<h3 className="sectionTitle" id="projects">My Projects</h3>
-					<p className="sectionSubTitle">1.Find Your Hat</p><p
-						className="des"
-					>
-						&nbsp;&nbsp;&nbsp;&nbsp;Interactive terminal game that
-						the player has lost their hat and they must navigate
-						back to it without falling in to the holes.
-						<a
-							className="projectlink"
-							href="https://replit.com/@PrachakTocheewe/find-yourhat#
-index.js"
-							target="_blank"
-						>
-							<br />
-							<button className="btnproject">view</button>
-						</a>
-					</p>
-
-					<p className="sectionSubTitle">2.Jammming</p><p className="des">
-					</p><p className="des">
-						&nbsp;&nbsp;&nbsp;&nbsp;React web application creating a
-						playlist with the Spotify API which allows users to
-						search the Spotify library, create a customized
-						playlist, then save it to their Spotify accounts.
-						<a
-							className="projectlink"
-							href="http://prachak32-jammming.surge.sh"
-							target="_blank"
-						>
-							<br />
-							<button className="btnproject">view</button>
-						</a>
-					</p>
-
-					<p className="sectionSubTitle">3.Infinity Health</p>
-					<p className="des">
-						&nbsp;&nbsp;&nbsp;&nbsp;An
-						exercise-tracker using MERN stack website which allows
-						users to keep records of their excercise
-						activities-adding, editing, deleting their activities
-						with reminders and their achieved milestones
-
-						<a
-							className="projectlink"
-							href="https://infinity-health.vercel.app/"
-							target="_blank"
-						>
-							<br />
-							<button className="btnproject">view</button>
-						</a>
-					</p>
-
-                    <p className="sectionSubTitle">4.Forecasting Electricity Power Demand in Metropolitan
-and Provincial Areas Using Decomposition Method with
-Seasonal ARIMA and Modified SARIMA Model</p>
-					<p className="des">
-						&nbsp;&nbsp;&nbsp;&nbsp;Final year project: Studied and compared time-series forecasting method
-4 method with 3 data set of electricity power demand in
-Thailand.
-
-
-					</p>
-					
-				</div>
-            
+  return (
+    <div className="Projects">
+      <div className="section">
+        <div className="sectionTitle" id="projects">
+          My Projects
         </div>
-    );
+        <div className="sectionSubTitle">1.Find Your Hat</div>
+        <p className="des">
+          &nbsp;&nbsp;&nbsp;&nbsp;Interactive terminal game that the player has
+          lost their hat and they must navigate back to it without falling in to
+          the holes.
+          <a
+            className="projectlink"
+            href="https://replit.com/@PrachakTocheewe/find-yourhat#
+index.js"
+            target="_blank"
+          >
+            <br />
+            <button className="btnproject">view</button>
+          </a>
+        </p>
+
+        <p className="sectionSubTitle">2.Jammming</p>
+        <p className="des"></p>
+        <p className="des">
+          &nbsp;&nbsp;&nbsp;&nbsp;React web application creating a playlist with
+          the Spotify API which allows users to search the Spotify library,
+          create a customized playlist, then save it to their Spotify accounts.
+          <a
+            className="projectlink"
+            href="http://prachak32-jammming.surge.sh"
+            target="_blank"
+          >
+            <br />
+            <button className="btnproject">view</button>
+          </a>
+        </p>
+
+        <p className="sectionSubTitle">3.Infinity Health</p>
+        <p className="des">
+          &nbsp;&nbsp;&nbsp;&nbsp;An exercise-tracker using MERN stack website
+          which allows users to keep records of their excercise
+          activities-adding, editing, deleting their activities with reminders
+          and their achieved milestones
+          <a
+            className="projectlink"
+            href="https://infinity-health.vercel.app/"
+            target="_blank"
+          >
+            <br />
+            <button className="btnproject">view</button>
+          </a>
+        </p>
+
+        <p className="sectionSubTitle">
+          4.Forecasting Electricity Power Demand in Metropolitan and Provincial
+          Areas Using Decomposition Method with Seasonal ARIMA and Modified
+          SARIMA Model
+        </p>
+        <p className="des">
+          &nbsp;&nbsp;&nbsp;&nbsp;Final year project: Studied and compared
+          time-series forecasting method 4 method with 3 data set of electricity
+          power demand in Thailand.
+        </p>
+      </div>
+    </div>
+  )
 }
 
-export default Projects;
+export default Projects
